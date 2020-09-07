b6bb335cd814890e84a374c242e2dd6dcfdf3a96
Dianka Levchenko <dianka_levchenko@inbox.ru>
2020-09-07 14:04:23 +0200

    Added bootdtrap to find-users-forms and relocated them

--- adminapp/adminapp/settings.py
+++ adminapp/adminapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'libraryapp',
 
-    'django_filters'
+    'django_filters',
+    'bootstrapform'
 ]
 
 MIDDLEWARE = [
--- adminapp/libraryapp/filters.py
+++ adminapp/libraryapp/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.forms import TextInput
 from django_filters import CharFilter, BooleanFilter
 
 from .models import *
@@ -6,13 +7,13 @@ from .models import *
 
 class UserFilter(django_filters.FilterSet):
 
-    first_name = CharFilter(field_name='first_name', lookup_expr='icontains', label='Имя')
-    last_name = CharFilter(field_name='last_name', lookup_expr='icontains', label='Фамилия')
+    first_name = CharFilter(field_name='first_name', lookup_expr='icontains', label='Имя') #, label='Имя'
+    last_name = CharFilter(field_name='last_name', lookup_expr='icontains', label='Фамилия') #, label='Фамилия')
 
-    phone_number = CharFilter(field_name='phone_number', lookup_expr='icontains', label='Контактный номер')
+    phone_number = CharFilter(field_name='phone_number', lookup_expr='icontains', label='Контактный номер')#, label='Контактный номер')
     # or NumberFilter
 
-    is_active = BooleanFilter(field_name='is_active', label='Активен')
+    is_active = BooleanFilter(field_name='is_active', label='Статус') # , label='Активен')
 
     class Meta:
         model = User
